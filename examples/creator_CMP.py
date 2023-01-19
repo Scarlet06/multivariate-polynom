52e0038203ee50208940b944634414dd69b6aa36
Scarlet06 <scar.flandre6@gmail.com>
2023-01-19 12:25:56 +0100

    I didn't realize I could do that

--- examples/creator_CMP.py
+++ examples/creator_CMP.py
@@ -23,28 +23,28 @@ class CMulty_XY(ComplexMultyPolynomial): "This class is used only for suggest wh
 class CMulty_XYZ(ComplexMultyPolynomial): "This class is used only for suggest which unknown the ComplexMultyPolynomial should have"
 
 #   -> 0
-cm0:CMulty_X = ComplexMultyPolynomial.zeroMulty("x")
+cm0 = CMulty_X.zeroMulty("x")
 #   -> 1
-cm1:CMulty_XY = ComplexMultyPolynomial.oneMulty("x","y")
+cm1 = CMulty_XY.oneMulty("x","y")
 
-#   -> 4*y^3 - 0.3333*x^5*y^2
-cm_dict:CMulty_XY = ComplexMultyPolynomial({'0-3':1+2j,'5-2':-1/3}, ('x','y'))
-#   -> 2*x + 2*C0
-cm_dict_integrals:CMulty_X = ComplexMultyPolynomial({'1':2j},('x',),{'0':['C0',2]})
+#   -> (1+2j)*y^3 - 0.3333*x^5*y^2
+cm_dict = CMulty_XY({'0-3':1+2j,'5-2':-1/3}, ('x','y'))
+#   -> (2j)*x + 2*C0
+cm_dict_integrals = CMulty_X({'1':2j},('x',),{'0':['C0',2]})
 
-#   -> 3*x^2 + y + 1
-cm_text:CMulty_XY = ComplexMultyPolynomial.fromText("(3j-4-7.7j+2)*3*x^2+1+y")
-#   -> x^5*y^7
-cm_text_unkn:CMulty_XYZ = ComplexMultyPolynomial.fromText("x^3*y^7*x^2+(-2j)",('z',))
-#   -> 3*x*y - 7 + C*x
-cm_text_integ:CMulty_XYZ = ComplexMultyPolynomial.fromText("-7+3*x*y+(1)*z^2",('y','x','z'),{'0-1-0':['C',1]})
+#   -> (-6-14.1j)*x^2 + y + 1
+cm_text = CMulty_XY.fromText("(3j-4-7.7j+2)*3*x^2+1+y")
+#   -> x^5*y^7 - (2j)
+cm_text_unkn = CMulty_XYZ.fromText("x^3*y^7*x^2+(-2j)",('z',))
+#   -> 3*x*y + z^2 - 7 + C*x
+cm_text_integ = CMulty_XYZ.fromText("-7+3*x*y+(1)*z^2",('y','x','z'),{'0-1-0':['C',1]})
 
 #   -> eg: x^2 - 2x, max deg is 2
-cm_random:CMulty_X = ComplexMultyPolynomial.random(2)
+cm_random = CMulty_X.random(2)
 #   -> eg: -3*x^3 + 4*y*x - 2, max deg is 3
-cm_random_unkn:CMulty_XY = ComplexMultyPolynomial.random(3,"y","x")
+cm_random_unkn = CMulty_XY.random(3,"y","x")
 #   -> eg: -x^2*y + 4*y*x, max deg is 4 but y can have max power 1
-cm_random_dict:CMulty_XY = ComplexMultyPolynomial.random(4,"x",y=1)
+cm_random_dict = CMulty_XY.random(4,"x",y=1)
 
 
 if __name__ == '__main__':
--- examples/creator_MP.py
+++ examples/creator_MP.py
@@ -23,28 +23,28 @@ class Multy_XY(MultyPolinomial): "This class is used only for suggest which unkn
 class Multy_XYZ(MultyPolinomial): "This class is used only for suggest which unknown the MultyPolinomial should have"
 
 #   -> 0
-m0:Multy_X = MultyPolinomial.zeroMulty("x")
+m0 = Multy_X.zeroMulty("x")
 #   -> 1
-m1:Multy_XY = MultyPolinomial.oneMulty("x","y")
+m1 = Multy_XY.oneMulty("x","y")
 
 #   -> 4*y^3 - 0.3333*x^5*y^2
-m_dict:Multy_XY = MultyPolinomial({'0-3':4,'5-2':-1/3}, ('x','y'))
+m_dict = Multy_XY({'0-3':4,'5-2':-1/3}, ('x','y'))
 #   -> 2*x + 2*C0
-m_dict_integrals:Multy_X = MultyPolinomial({'1':2},('x',),{'0':['C0',2]})
+m_dict_integrals = Multy_X({'1':2},('x',),{'0':['C0',2]})
 
 #   -> 3*x^2 + y + 1
-m_text:Multy_XY = MultyPolinomial.fromText("3*x^2+1+y")
+m_text = Multy_XY.fromText("3*x^2+1+y")
 #   -> x^5*y^7
-m_text_unkn:Multy_XYZ = MultyPolinomial.fromText("x^3*y^7*x^2",('z',))
+m_text_unkn = Multy_XYZ.fromText("x^3*y^7*x^2",('z',))
 #   -> 3*x*y - 7 + C*x
-m_text_integ:Multy_XYZ = MultyPolinomial.fromText("-7+3*x*y",('y','x','z'),{'0-1-0':['C',1]})
+m_text_integ = Multy_XYZ.fromText("-7+3*x*y",('y','x','z'),{'0-1-0':['C',1]})
 
 #   -> eg: x^2 - 2x, max deg is 2
-m_random:Multy_X = MultyPolinomial.random(2)
+m_random = Multy_X.random(2)
 #   -> eg: -3*x^3 + 4*y*x - 2, max deg is 3
-m_random_unkn:Multy_XY = MultyPolinomial.random(3,"y","x")
+m_random_unkn = Multy_XY.random(3,"y","x")
 #   -> eg: -x^2*y + 4*y*x, max deg is 4 but y can have max power 1
-m_random_dict:Multy_XY = MultyPolinomial.random(4,"x",y=1)
+m_random_dict = Multy_XY.random(4,"x",y=1)
 
 
 if __name__ == '__main__':
